a1f09ad2d4e9eb3deb9c7d2122e6831ce5fb1601
Ryan Elley <ryan.elley@ecan.govt.nz>
2017-05-28 15:10:24 +1200

    SmartEditorEcan & EditFilter - Workspaces set top for SmartEditorEcan and EditFilter widgets, which are copies of SmartEditor and GroupFilter widgets respectively.  Bug fix in EditFilter widget for query extend method being passed NaN on empty extent when no results are returned after filter applied.

---
+++ dist/widgets/EditFilter/VersionManager.js
@@ -0,0 +1,30 @@
+define(['jimu/shared/BaseVersionManager'], function (BaseVersionManager) {
+  function VersionManager() {
+    this.versions = [{
+      version: '2.1',
+      upgrader: function upgrader(oldConfig) {
+        return oldConfig;
+      }
+    }, {
+      version: '2.2',
+      upgrader: function upgrader(oldConfig) {
+        var newConfig = oldConfig;
+        newConfig.webmapAppendMode = false;
+        newConfig.slAppendChoice = 'OR';
+        newConfig.zoomMode = true;
+        for (var i = 0; i < newConfig.groups.length; i++) {
+          newConfig.groups[i].appendSameLayer = false;
+          newConfig.groups[i].appendSameLayerConjunc = 'OR';
+          for (var z = 0; z < newConfig.groups[i].layers.length; z++) {
+            newConfig.groups[i].layers[z].useZoom = false;
+          }
+        }
+        return newConfig;
+      }
+    }];
+  }
+
+  VersionManager.prototype = new BaseVersionManager();
+  VersionManager.prototype.constructor = VersionManager;
+  return VersionManager;
+});
