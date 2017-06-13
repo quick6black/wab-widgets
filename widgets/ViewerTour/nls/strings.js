dcf5ed9edb45a7632cb84a5b47bbb13265ab91e0
Ryan Elley <ryan.elley@ecan.govt.nz>
2017-06-13 20:35:11 +1200

    Tour Widget - Initial Setup.  Does not work at this stage as missing dojo bootstrap port files.

---
+++ widgets/ViewerTour/nls/strings.js
@@ -0,0 +1,8 @@
+define({
+  root: {
+    widgetTitle: 'Viewer Tour',
+    description: 'Widget utilising tour.js to give a tour of the web appbuilder application.'
+  }
+  // add supported locales below:
+  // , "zh-cn": true
+});
