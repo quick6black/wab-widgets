f31973ad2ce5954822c40b9da1c72f070f1a344d
CH\RyanE <ryan.elley@ecan.govt.nz>
2018-03-12 10:40:08 +1300

    Viewer Tour - Widget development abandoned.  Clean up of unnecessary code.

--- widgets/ViewerTour/nls/strings.js
+++
@@ -1,8 +0,0 @@
-define({
-  root: {
-    widgetTitle: 'Viewer Tour',
-    description: 'Widget utilising tour.js to give a tour of the web appbuilder application.'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
